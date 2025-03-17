05af633f3b0534004c2ee4f078c61b925091672f
Leandro Santana <96756735+Leandro-Santana013@users.noreply.github.com>
2025-03-17 03:27:13 -0300

    exmilha

---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("coloque a metragem em milha pra converter para km/h:");
+Console.WriteLine(double.Parse(Console.ReadLine()) * 1.609 + "km/h");
